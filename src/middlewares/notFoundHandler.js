aec4b121268821a31ee50385ca2c220f6d79539c
Xelle <oksana2854x@gmail.com>
2025-04-02 06:39:07 +0200

    fix

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,6 +1,5 @@
 import createError from 'http-errors';
 
 export const notFoundHandler = (req, res, next) => {
-  const error = createError(404, 'Route not found');
-  next(error);
+  next(createError(404, 'Route not found'));
 };
