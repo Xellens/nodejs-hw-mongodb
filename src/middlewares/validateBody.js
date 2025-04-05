4d1f803b80635d9fc7ed5388d868253ba475bc0f
Xelle <oksana2854x@gmail.com>
2025-04-05 06:48:18 +0200

    fix validateBody

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -2,9 +2,11 @@ import createError from 'http-errors';
 
 export const validateBody = (schema) => {
   return (req, res, next) => {
-    const { error } = schema.validate(req.body);
+    const { error } = schema.validate(req.body, { abortEarly: false });
+
     if (error) {
-      return next(createError(400, error.details[0].message));
+      const allMessages = error.details.map((detail) => detail.message);
+      return next(createError(400, allMessages.join('. ')));
     }
     next();
   };
